6aa3e0fdf5b15bf93b509dc68baf7fa8a47dc7f5
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2017-09-25 11:11:56 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,36 +6,50 @@ const is = require('is')
  * @param {string} path path of folder with routers for express.js
  * @returns {express.Router}
  */
-module.exports = function (path) {
+module.exports = (path, recursive = false) => {
   /**
    * @type {{ [ key: number ]: Function }}
    */
-  const routes = dir(path, { recursive: true })
+  const routes = dir(path, { recursive })
+
   /**
    * @type {express.Router}
    */
   const middleware = express()
+
   // load routes into express instance
   for (const namespace of Object.keys(routes)) {
     /**
     * @type {string}
     */
     const path = `/${namespace}`.toLowerCase()
+
     /**
      * @type {Function}
      */
-    const route = routes[ namespace ].default
+    let route = routes[ namespace ].default
+
     // check if middleware is valid
     if (!is.function(route)) {
       throw new Error(`${namespace} not is an valid middleware`)
     }
-    /**
-     * @type {Function}
-     */
-    const handler = /^\s*class\s+/.test(route.toString()) ? new route().router() : route()
+
+    if (/^\s*class\s+/.test(route.toString())) {
+      const instance = new route()
+
+      if ('didMount' in instance) {
+        route = instance.didMount()
+      }
+
+      if ('router' in instance) {
+        route = instance.router()
+      }
+    }
+
     // put middleware in express
-    middleware.use(path, handler)
+    middleware.use(path, route)
   }
+
   // returns all routes loaded
   return middleware
 }
