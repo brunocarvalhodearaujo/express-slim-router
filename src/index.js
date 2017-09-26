43e28c7d47eeec5a6289316621c9e459b718c707
Bruno Carvalho de Araujo <carvalho@hostnet.com.br>
2017-09-26 13:53:10 -0300

    changes in internal api

--- src/index.js
+++ src/index.js
@@ -1,55 +1,37 @@
 const express = require('express')
-const dir = require('require-dir')
-const is = require('is')
+const glob = require('require-glob')
 
 /**
  * @param {string} path path of folder with routers for express.js
- * @returns {express.Router}
+ * @returns {express.Router[]}
  */
-module.exports = (path, recursive = false) => {
-  /**
-   * @type {{ [ key: number ]: Function }}
-   */
-  const routes = dir(path, { recursive })
+module.exports = (path) => {
+  const packages = glob.sync(`${path}/**/*.js`)
+  const router = []
 
-  /**
-   * @type {express.Router}
-   */
-  const middleware = express()
+  for (const routes of Object.values(packages)) {
+    for (const path of Object.keys(routes)) {
+      let route = routes[ path ]
 
-  // load routes into express instance
-  for (const namespace of Object.keys(routes)) {
-    /**
-    * @type {string}
-    */
-    const path = `/${namespace}`.toLowerCase()
-
-    /**
-     * @type {Function}
-     */
-    let route = routes[ namespace ].default
-
-    // check if middleware is valid
-    if (!is.function(route)) {
-      throw new Error(`${namespace} not is an valid middleware`)
-    }
-
-    if (/^\s*class\s+/.test(route.toString())) {
-      const instance = new route()
-
-      if ('didMount' in instance) {
-        route = instance.didMount()
+      if (typeof route !== 'function') {
+        throw new Error(`${path} not is an valid middleware`)
       }
 
-      if ('router' in instance) {
-        route = instance.router()
-      }
-    }
+      if (/^\s*class\s+/.test(route.toString())) {
+        const instance = new route() // eslint-disable-line
 
-    // put middleware in express
-    middleware.use(path, route)
+        if ('didMount' in instance) {
+          route = instance.didMount()
+        }
+
+        if ('router' in instance) {
+          route = instance.router()
+        }
+      }
+
+      router.push([ `/${path}`.toLowerCase(), route ])
+    }
   }
 
-  // returns all routes loaded
-  return middleware
+  return router.map(([ path, route ]) => express().use(path, route))
 }
